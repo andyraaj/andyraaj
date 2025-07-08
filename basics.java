0773a73291706173903c2667e3e4ba606ecf8b8a
Anand Raj Barnwal <anandrajbarnwal@gmail.com>
2025-07-08 21:44:44 +0530

    add initial files

---
+++ basics.java
@@ -0,0 +1,7 @@
+public class basics {
+
+    public static int main(String[] args) {
+        System.out.println("Hello World");
+        return 0;
+    }
+}
---
+++ numbertypes.java
@@ -0,0 +1,13 @@
+public class numbertypes {
+    public static void main(String[] args) {
+        // -128 to 127
+        // byte stores 8bits in memory
+        byte num = 100;
+        System.out.println(num);
+        //short takes 16 bits 
+        short a=2000;
+        System.out.println(a);
+        //long takes 64 bi
+
+    }
+}
